eb587bb684afe434aba3e5d9755c01032b0ce313
mixedup4x4 <mixedup4x4@gmail.com>
2014-02-27 20:56:31 -0500

    Unit 8 added

---
+++ IT193_Unit8_JasonChapell/IT193_Unit8_JasonChapell/frmCountries.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProject
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmProject))
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblJason = New System.Windows.Forms.Label()
+        Me.lblClass = New System.Windows.Forms.Label()
+        Me.gpbCountry = New System.Windows.Forms.GroupBox()
+        Me.txtCountryName = New System.Windows.Forms.TextBox()
+        Me.txtAbbreviation = New System.Windows.Forms.TextBox()
+        Me.rdoCountryName = New System.Windows.Forms.RadioButton()
+        Me.rdoAbbreviation = New System.Windows.Forms.RadioButton()
+        Me.btnLookup = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.gpbCountry.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnExit.Location = New System.Drawing.Point(421, 412)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(97, 32)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblJason
+        '
+        Me.lblJason.AutoSize = True
+        Me.lblJason.Location = New System.Drawing.Point(477, 502)
+        Me.lblJason.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblJason.Name = "lblJason"
+        Me.lblJason.Size = New System.Drawing.Size(172, 16)
+        Me.lblJason.TabIndex = 1
+        Me.lblJason.Text = "Programmed by: Jason Chapell"
+        '
+        'lblClass
+        '
+        Me.lblClass.AutoSize = True
+        Me.lblClass.Location = New System.Drawing.Point(490, 523)
+        Me.lblClass.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblClass.Name = "lblClass"
+        Me.lblClass.Size = New System.Drawing.Size(172, 16)
+        Me.lblClass.TabIndex = 2
+        Me.lblClass.Text = "Kaplan University - IT193-02"
+        '
+        'gpbCountry
+        '
+        Me.gpbCountry.Controls.Add(Me.txtCountryName)
+        Me.gpbCountry.Controls.Add(Me.txtAbbreviation)
+        Me.gpbCountry.Controls.Add(Me.rdoCountryName)
+        Me.gpbCountry.Controls.Add(Me.rdoAbbreviation)
+        Me.gpbCountry.Location = New System.Drawing.Point(126, 127)
+        Me.gpbCountry.Name = "gpbCountry"
+        Me.gpbCountry.Size = New System.Drawing.Size(392, 237)
+        Me.gpbCountry.TabIndex = 3
+        Me.gpbCountry.TabStop = False
+        Me.gpbCountry.Text = "Select How You Want To Search"
+        '
+        'txtCountryName
+        '
+        Me.txtCountryName.Location = New System.Drawing.Point(225, 126)
+        Me.txtCountryName.Name = "txtCountryName"
+        Me.txtCountryName.Size = New System.Drawing.Size(100, 23)
+        Me.txtCountryName.TabIndex = 3
+        '
+        'txtAbbreviation
+        '
+        Me.txtAbbreviation.Location = New System.Drawing.Point(225, 59)
+        Me.txtAbbreviation.Name = "txtAbbreviation"
+        Me.txtAbbreviation.Size = New System.Drawing.Size(100, 23)
+        Me.txtAbbreviation.TabIndex = 2
+        '
+        'rdoCountryName
+        '
+        Me.rdoCountryName.AutoSize = True
+        Me.rdoCountryName.Location = New System.Drawing.Point(26, 127)
+        Me.rdoCountryName.Name = "rdoCountryName"
+        Me.rdoCountryName.Size = New System.Drawing.Size(92, 17)
+        Me.rdoCountryName.TabIndex = 1
+        Me.rdoCountryName.TabStop = True
+        Me.rdoCountryName.Text = "Country Name"
+        Me.rdoCountryName.UseVisualStyleBackColor = True
+        '
+        'rdoAbbreviation
+        '
+        Me.rdoAbbreviation.AutoSize = True
+        Me.rdoAbbreviation.Location = New System.Drawing.Point(26, 59)
+        Me.rdoAbbreviation.Name = "rdoAbbreviation"
+        Me.rdoAbbreviation.Size = New System.Drawing.Size(84, 17)
+        Me.rdoAbbreviation.TabIndex = 0
+        Me.rdoAbbreviation.TabStop = True
+        Me.rdoAbbreviation.Text = "Abbreviation"
+        Me.rdoAbbreviation.UseVisualStyleBackColor = True
+        '
+        'btnLookup
+        '
+        Me.btnLookup.Location = New System.Drawing.Point(138, 421)
+        Me.btnLookup.Name = "btnLookup"
+        Me.btnLookup.Size = New System.Drawing.Size(75, 23)
+        Me.btnLookup.TabIndex = 4
+        Me.btnLookup.Text = "Lookup"
+        Me.btnLookup.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(295, 421)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'frmProject
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlText
+        Me.ClientSize = New System.Drawing.Size(668, 550)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnLookup)
+        Me.Controls.Add(Me.gpbCountry)
+        Me.Controls.Add(Me.lblClass)
+        Me.Controls.Add(Me.lblJason)
+        Me.Controls.Add(Me.btnExit)
+        Me.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "frmProject"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ProjectTitle"
+        Me.gpbCountry.ResumeLayout(False)
+        Me.gpbCountry.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblJason As System.Windows.Forms.Label
+    Friend WithEvents lblClass As System.Windows.Forms.Label
+    Friend WithEvents gpbCountry As System.Windows.Forms.GroupBox
+    Friend WithEvents txtCountryName As System.Windows.Forms.TextBox
+    Friend WithEvents txtAbbreviation As System.Windows.Forms.TextBox
+    Friend WithEvents rdoCountryName As System.Windows.Forms.RadioButton
+    Friend WithEvents rdoAbbreviation As System.Windows.Forms.RadioButton
+    Friend WithEvents btnLookup As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ IT193_Unit8_JasonChapell/IT193_Unit8_JasonChapell/frmCountries.vb
@@ -0,0 +1,154 @@
+﻿'========================================================= 
+
+' <Jason Chapell>
+
+' EMAIL: <jasonchapell@student.kaplan.edu>
+
+' Operating System Version Windows XP Pro 64-Bit in a VM on a MAC
+
+' Your Compiler (Visual Studio 2010 Ultimate) 
+
+' Course IT193 Section 02 
+
+' Unit 8 Project 
+
+' Program Description: Search by Abbreviation or Name
+
+' Academic Honesty: 
+
+' I attest that this is my original work. 
+
+' I have not used unauthorized source code, either modified or unmodified. 
+
+' I have not given other fellow student(s) access to my program. 
+
+'=========================================================== 
+Public Class frmProject
+    'declare structure
+    Structure Country
+        Dim Name As String
+        Dim Abbreviation As String
+    End Structure
+
+    'declare array
+    Dim TableOfCountries(8) As Country
+
+
+
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        'close program
+        Me.Close()
+
+    End Sub
+
+    Private Sub rdoAbbreviation_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles rdoAbbreviation.CheckedChanged, rdoCountryName.CheckedChanged
+        Dim RB As RadioButton
+
+        'determine which button was selected
+        RB = CType(sender, RadioButton)
+
+        'use case statement to set one to read only
+        Select Case RB.Name
+            Case "rdoAbbreviation"
+                txtAbbreviation.ReadOnly = False
+                txtCountryName.ReadOnly = True
+                txtAbbreviation.Focus()
+            Case "rdoCountryName"
+                txtCountryName.ReadOnly = False
+                txtAbbreviation.ReadOnly = True
+                txtCountryName.Focus()
+
+        End Select
+
+    End Sub
+
+    Private Sub btnLookup_Click(sender As System.Object, e As System.EventArgs) Handles btnLookup.Click
+        'declare variable
+        Dim counter As Integer
+        Dim found As Boolean
+
+        'if abbreviation is checked 
+        If rdoAbbreviation.Checked Then
+            While (counter <= TableOfCountries.GetUpperBound(0)) And Not found
+
+
+                'compare the abbreviation text box with element of array
+                If txtAbbreviation.Text.ToUpper = TableOfCountries(counter).Abbreviation.toUpper Then
+                    'if a match 
+                    'set name to corresponding name
+                    txtCountryName.Text = TableOfCountries(counter).Name
+                    found = True
+                Else
+                    'if not a match
+                    'loop to next item in array
+                    counter += 1
+                End If
+            End While
+
+        Else
+
+        End If
+
+
+
+        'if name is checked
+
+        If rdoCountryName.Checked Then
+            While (counter <= 8) And Not found
+
+
+                'compare the abbreviation text box with element of array
+                If txtCountryName.Text.ToUpper = TableOfCountries(counter).Name.ToUpper Then
+                    'if a match 
+                    'set name to corresponding name
+                    txtAbbreviation.Text = TableOfCountries(counter).Abbreviation
+                    found = True
+                Else
+                    'if not a match
+                    'loop to next item in array
+                    counter += 1
+                End If
+            End While
+        End If
+
+
+        'if item not found
+        'display error
+        If Not found Then
+            MsgBox("HEY THERE WHAT DID YOU DO", vbCritical, "OOPS")
+
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As System.Object, e As System.EventArgs) Handles btnClear.Click
+        'clear both text boxes
+        txtAbbreviation.Clear()
+        txtCountryName.Clear()
+
+        'set abbreviation to checked
+        rdoAbbreviation.Focus()
+
+    End Sub
+
+    Private Sub frmProject_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        TableOfCountries(0).Abbreviation = "US"
+        TableOfCountries(0).Name = "United States"
+        TableOfCountries(1).Abbreviation = "GE"
+        TableOfCountries(1).Name = "Germany"
+        TableOfCountries(2).Abbreviation = "RU"
+        TableOfCountries(2).Name = "Russia"
+        TableOfCountries(3).Abbreviation = "IQ"
+        TableOfCountries(3).Name = "Iraq"
+        TableOfCountries(4).Abbreviation = "MX"
+        TableOfCountries(4).Name = "Mexico"
+        TableOfCountries(5).Abbreviation = "UK"
+        TableOfCountries(5).Name = "United Kingdom"
+        TableOfCountries(6).Abbreviation = "IR"
+        TableOfCountries(6).Name = "Ireland"
+        TableOfCountries(7).Abbreviation = "BZ"
+        TableOfCountries(7).Name = "Brazil"
+        TableOfCountries(8).Abbreviation = "IT"
+        TableOfCountries(8).Name = "Italy"
+    End Sub
+End Class
